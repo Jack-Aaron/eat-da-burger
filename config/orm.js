403bb5b215f4354314c7c58760d8880390fbc434
Jonathan Alpart <panjialang@gmail.com>
2020-05-29 19:10:49 -0500

    hbs buttons

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,40 @@
 var connection = require("./connection.js");
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+          // if string with spaces, add quotations 
+          if (typeof value === "string" && value.indexOf(" ") >= 0) {
+              value = "'" + value + "'";
+          }
+          arr.push(key + "=" + value);
+      }
+  }
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 var orm = {
-  selectAll: function (tableInput, cb) {
+  selectAll: function (table, cb) {
     var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], cb)
+    connection.query(queryString, [table], cb)
   },
-  insertOne: function (whatToInsert, table, orderCol) {
-    var queryString = "INSERT INTO ?? SET ?? = ?";
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO ?? VALUES = ?";
     console.log(queryString);
-    connection.query(queryString, [whatToInsert, table, orderCol], cb)
+    connection.query(queryString, [table, cols, vals], cb)
   },
-  updateOne: function (whatToUpdate, table, condition) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString =
       "UPDATE ?? SET ?? WHERE ?? = ?";
     connection.query(
       queryString,
-      [whatToUpdate, table, condition], cb);
+      [table, objToSql(objColVals), condition], cb);
   }
 };
 
